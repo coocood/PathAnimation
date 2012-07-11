765f5b30acfc645785178d3d1db67b53a1312003
Ewan Chou <coocood@gmail.com>
2012-07-11 09:31:10 +0800

    first commmit

---
+++ PathAnimation.java
@@ -0,0 +1,21 @@
+package com.coolands.open;
+
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+public class PathAnimation extends Animation {
+	private PathMeasure measure;
+	private float[] pos = new float[2];
+	public PathAnimation(Path path) {
+		measure = new PathMeasure(path, false);
+		measure.getPosTan(0, pos, null);
+	}
+	
+	@Override
+	protected void applyTransformation(float interpolatedTime, Transformation t){
+		measure.getPosTan(measure.getLength() * interpolatedTime, pos,null);
+		t.getMatrix().setTranslate(pos[0], pos[1]);
+	}
+}
